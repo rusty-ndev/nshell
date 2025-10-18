0be5e72b2cc398f67962c2229fc22a6aea68fd24
rusty-ndev <npatel08075@gmail.com>
2025-10-18 15:26:50 +0100

    Added read functionality

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LSH_RL_BUFSIZE 1024 
+
 void sh_loop(void){
 
 	char *line; 
@@ -16,6 +21,46 @@ void sh_loop(void){
 		
 }
 
+char *sh_read_line(void){
+
+	int bufsize = LSH_RL_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize); 
+	int c; 
+	
+	if(!buffer){
+		fprintf(stderr, "sh: allocation error\n"); 
+		exit(EXIT_FAILURE); 
+	}
+
+	while(1){
+		// read character 
+		c = getchar(); 
+
+		// if we hit EOF, replace it with null character and return
+		if(c == EOF|| c== '\n')
+		{
+			buffer[position] = '\0';
+			return buffer; 
+		}else{
+			buffer[position] = c; 
+		}
+		position ++; 
+
+		//if we have exceeded the buffer, reallocate
+		if(position >= bufsize ){
+			bufsize += LSH_RL_BUFSIZE; 
+			buffer = realloc(buffer, bufsize); 
+			if(!buffer){
+				fprintf(stderr, "sh: allocation error\n"); 
+				exit(EXIT_FAILURE); 
+			}
+		}
+	} 
+ 
+}
+
+
 
 int main(int argc, char **argv){
 	// config file loading
@@ -27,3 +72,4 @@ int main(int argc, char **argv){
 	return EXIT_SUCCESS; 
 	
 }
+ 
