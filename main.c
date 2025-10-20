7fa85f4b2ee99b3159cfa04b925a2482e8d60a5b
rusty-ndev <npatel08075@gmail.com>
2025-10-20 21:18:07 +0100

    Added a launch function to make child processes

--- main.c
+++ main.c
@@ -95,6 +95,30 @@ char *sh_read_line(void){
  
 }
 
+int sh_launch(char **args){
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
 
 
 int main(int argc, char **argv){
