48283cfb21f7c33b4611556591adec0dc66101db
rusty-ndev <npatel08075@gmail.com>
2025-10-21 23:16:27 +0100

    Added execute

--- main.c
+++ main.c
@@ -5,13 +5,19 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SH_RL_BUFSIZE 1024 
-#define SH_TOK_BUFSIZE 64 
-#define SH_TOK_DELIM " \t\r\n\a"
+#define nsh_RL_BUFSIZE 1024 
+#define nsh_TOK_BUFSIZE 64 
+#define nsh_TOK_DELIM " \t\r\n\a"
 
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
+void nsh_loop(void);
+char *nsh_read_line(void);
+char **nsh_split_line(char *line);
+int nsh_execute(char **args);
+int nsh_launch(char **args);
+
+int nsh_cd(char **args);
+int nsh_help(char **args);
+int nsh_exit(char **args);
 
 char *builtin_str[] = {
   "cd",
@@ -20,16 +26,50 @@ char *builtin_str[] = {
 };
 
 int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
+  &nsh_cd,
+  &nsh_help,
+  &nsh_exit
 };
 
-int lsh_num_builtins() {
+int nsh_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
-void sh_loop(void){
+int nsh_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "nsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("nsh");
+    }
+  }
+  return 1;
+}
+
+
+int nsh_help(char **args)
+{
+  int i;
+  printf("Naeem's nsh\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < nsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+
+int nsh_exit(char **args)
+{
+  return 0;
+}
+
+void nsh_loop(void){
 
 	char *line; 
 	char **args; 
@@ -37,9 +77,9 @@ void sh_loop(void){
 
 	do{
 		printf("> "); 
-		line = sh_read_line(); 
-		args = sh_split_line(line);
-		status = sh_execute; 
+		line = nsh_read_line(); 
+		args = nsh_split_line(line);
+		status = nsh_execute(args); 
 
 		free(line); 
 		free(args); 
@@ -47,47 +87,47 @@ void sh_loop(void){
 		
 }
 
-char** sh_split_line(char* line){
+char** nsh_split_line(char* line){
 
-	int bufsize = SH_TOK_BUFSIZE , position = 0 ; 
+	int bufsize = nsh_TOK_BUFSIZE , position = 0 ; 
     char **tokens = malloc(bufsize * sizeof(char*));
     char *token;
 
   if (!tokens) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "nsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  token = strtok(line, SH_TOK_DELIM);
+  token = strtok(line, nsh_TOK_DELIM);
   while (token != NULL) {
     tokens[position] = token;
     position++;
 
     if (position >= bufsize) {
-      bufsize += SH_TOK_BUFSIZE;
+      bufsize += nsh_TOK_BUFSIZE;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
-        fprintf(stderr, "lsh: allocation error\n");
+        fprintf(stderr, "nsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
 
-    token = strtok(NULL, SH_TOK_DELIM);
+    token = strtok(NULL, nsh_TOK_DELIM);
   }
   tokens[position] = NULL;
   return tokens;
 } 
 
 
-char* sh_read_line(void){
+char* nsh_read_line(void){
 
-	int bufsize = SH_RL_BUFSIZE;
+	int bufsize = nsh_RL_BUFSIZE;
 	int position = 0;
 	char *buffer = malloc(sizeof(char) * bufsize); 
 	int c; 
 	
 	if(!buffer){
-		fprintf(stderr, "sh: allocation error\n"); 
+		fprintf(stderr, "nsh: allocation error\n"); 
 		exit(EXIT_FAILURE); 
 	}
 
@@ -107,10 +147,10 @@ char* sh_read_line(void){
 
 		//if we have exceeded the buffer, reallocate
 		if(position >= bufsize ){
-			bufsize += SH_RL_BUFSIZE; 
+			bufsize += nsh_RL_BUFSIZE; 
 			buffer = realloc(buffer, bufsize); 
 			if(!buffer){
-				fprintf(stderr, "sh: allocation error\n"); 
+				fprintf(stderr, "nsh: allocation error\n"); 
 				exit(EXIT_FAILURE); 
 			}
 		}
@@ -118,7 +158,7 @@ char* sh_read_line(void){
  
 }
 
-int sh_launch(char **args){
+int nsh_launch(char **args){
   pid_t pid, wpid;
   int status;
 
@@ -126,12 +166,12 @@ int sh_launch(char **args){
   if (pid == 0) {
     // Child process
     if (execvp(args[0], args) == -1) {
-      perror("lsh");
+      perror("nsh");
     }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
     // Error forking
-    perror("lsh");
+    perror("nsh");
   } else {
     // Parent process
     do {
@@ -142,15 +182,33 @@ int sh_launch(char **args){
   return 1;
 }
 
+int nsh_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < nsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return nsh_launch(args);
+}
+
 
 
 int main(int argc, char **argv){
 	// config file loading
 
 	// command loop
-	sh_loop();
+	nsh_loop();
 
-	// shutdown/cleanup
+	// nshutdown/cleanup
 	return EXIT_SUCCESS; 
 	
 }
